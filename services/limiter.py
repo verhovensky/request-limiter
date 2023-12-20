9131f70f1468dd4648440f4ad126c5bb7f0c48f1
verhovensky <voinywolnyprod@gmail.com>
2023-12-20 03:16:38 +0300

    Добавил async redis в зависимости * Добавил docker-compose.prod.yml * Рефакторинг docker-compose.dev.yml * Дополнил env_example, config.py * Дополнил limiter.py

--- services/limiter.py
+++ services/limiter.py
@@ -1,7 +1,17 @@
+from hashlib import sha256
+
+
 class LimiterService:
     @staticmethod
-    def _make_hash(data: dict):
+    def check_rate_limit():
+        """Проверяет текущее количество запущенных тасок.
+        Этот ключ лежит в Redis и увеличивается/уменьшается в зависимости от того,
+        Выполнена таска или только поступила."""
+
+    @staticmethod
+    def _make_hash(data: dict) -> str:
         """Вычисляет уникальный хеш из всех данных запроса (SHA-256)"""
+        return sha256(bytes(str(data), "utf-8")).hexdigest()
 
     @staticmethod
     def check_duplicated_request():
@@ -9,9 +19,9 @@ class LimiterService:
         - если находит такой же хеш, то пишет в лог о дубликате запроса
         (и не вызывает передачу запроса в очеред Rabbit MQ)
         - если не находит такой же хеш, то делает хеш, кладет в Redis с TTL
-        (вызывает передачу запроса в очеред Rabbit MQ)"""
+        (вызывает передачу запроса в очередь)"""
 
     @staticmethod
     def process_request():
         """Сначала делает хеш, проверяет нет ли такого же,
-        затем отправляет запрос в очередь Rabbit MQ"""
+        затем отправляет запрос в очередь"""
--- settings/config.py
+++ settings/config.py
@@ -1,3 +1,8 @@
+import sys
+
+import redis.asyncio as redis
+from pydantic import field_validator
+from pydantic_core.core_schema import ValidationInfo
 from pydantic_settings import BaseSettings
 
 
@@ -12,6 +17,29 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: list[str] = []
     # Localhost by default
     ALLOWED_HOSTS: list[str] = ["localhost", "127.0.0.1"]
+
+    # Redis
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_PASS: str | None = None
+    REDIS_CLIENT: redis.Redis | None = None
+
+    @field_validator("REDIS_CLIENT")
+    def _assemble_redis_client(cls, v: str, values: ValidationInfo) -> redis.Redis:
+        try:
+            if values.data["REDIS_PASS"] is not None or values.data["REDIS_PASS"] != "":
+                return redis.Redis(
+                    host=values.data["REDIS_HOST"],
+                    port=values.data["REDIS_PORT"],
+                    password=values.data["REDIS_PASS"],
+                    db=0,
+                )
+            else:
+                return redis.Redis(
+                    host=values.data["REDIS_HOST"], port=values.data["REDIS_PORT"], db=0
+                )
+        except Exception as e:
+            sys.stdout.write(f"Redis initialization failed! {e}")
 
     class Config:
         env_file = ".env"
