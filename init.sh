8fe51a2d247efa483fab9ebe74d4a45bc846411a
verhovensky <voinywolnyprod@gmail.com>
2023-12-20 18:34:14 +0300

    Поправил запуск gunicorn * убрал дебаг проверки

--- init.sh
+++ init.sh
@@ -5,14 +5,10 @@ if [ "$1" = "run_celery" ]; then
 fi
 
 if [ "$1" = "run_flower" ]; then
-   python --version && which python
-   python -m celery
   celery -A worker.celery flower --port=5555 --loglevel=debug
 fi
 
 if [ "$1" = "run_server" ]; then
-  python --version && which python
   echo "Run server"
-  exec gunicorn main:app --preload --config file:gunicorn.conf.py -b 0.0.0.0:5000
-  # exec gunicorn app.main:app --config file:gunicorn.conf.py
+  exec gunicorn app.main:app --config file:gunicorn.conf.py
 fi
